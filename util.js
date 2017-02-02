012c9fad3e23668e9953566aaeb7882e2660b553
Alvin Kerber <akerber47@gmail.com>
2017-02-02 11:30:52 -0800

    Implemented utility objs/funs for raw images,gamma
    Test image (alternating blue / gradient checkerboard) looks
    good like in the book!

--- util.js
+++ util.js
@@ -34,12 +34,77 @@ function Ray(origin,direction) {
     this.direction = direction || new Color3(); // Vector3
 }
 
+function RawImage(width,height) {
+    this.width = width;
+    this.height = height;
+    this.data = []; // array of Radiance3
+}
+
+RawImage.prototype.get = function(x,y) {
+    return this.data[x + this.width * y];
+};
+
+RawImage.prototype.set = function(x,y,rad) {
+    this.data[x + this.width * y] = rad;
+};
+
 /* Gamma encoding w/ gamma = 2.2 (sRGB standard curve).
  * scale should multiply radiance to between 0.0 and 1.0.
  */
 function ppmGammaEncode(radiance, scale) {
     const gamma = 2.2;
     return Math.round(Math.pow(
-        Math.min([1.0, Math.max([0.0, radiance * scale])]),
+        Math.min(1.0, Math.max(0.0, radiance * scale)),
         1.0 / gamma) * 255);
+}
+
+/* Convert raw mathematically generated image (array of Radiance3 objects)
+ * to rgb image data, 0-255. Use ppm gamma encoding.
+ * Pass in imagedata to fill (from a createImageData call, probably). Modifies
+ * the given imageData IN PLACE!
+ */
+function encodeImageData(rawImage, scale, imageData) {
+    for (var y = 0; y < rawImage.height; y++) {
+        for (var x = 0; x < rawImage.width; x++) {
+            var px = x + rawImage.width * y;
+            imageData.data[4*px+0] = ppmGammaEncode(rawImage.get(x,y).r, scale);
+            imageData.data[4*px+1] = ppmGammaEncode(rawImage.get(x,y).g, scale);
+            imageData.data[4*px+2] = ppmGammaEncode(rawImage.get(x,y).b, scale);
+            imageData.data[4*px+3] = 255; // alpha = 1.0 (opaque)
+        }
+    }
+}
+
+// Test image from the book
+function initTestImage() {
+    var testImage = new RawImage(100,100);
+    for (var y = 0; y < 100; y++) {
+        for (var x = 0; x < 100; x++) {
+            if ((x+y) % 2) {
+                testImage.set(x, y, {r: 0, g: 0, b: 0.1});
+            } else {
+                testImage.set(x, y, {r: (y/100), g: (y/100), b: (y/100)});
+            }
+        }
+    }
+    return testImage;
+}
+
+
+/* Convert image data to a long string (in PPM file format).
+ * Return the string.
+ */
+function imageDataToPpm(imageData) {
+    var output = "";
+    output += "P3 " + imageData.width + " " + imageData.height + " 255\n";
+    for (var y = 0; y < imageData.height; y++) {
+        output += "\n # y = " + y + "\n";
+        for (var x = 0; x < imageData.width; x++) {
+            var px = x + imageData.width * y;
+            output += imageData.data[px] + " " +
+                imageData.data[px+1] + " " +
+                imageData.data[px+2] + "\n";
+        }
+    }
+    return output;
 }
