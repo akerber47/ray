4b09806951df467190ed5d00078d263331abdaa4
Alvin Kerber <akerber47@gmail.com>
2017-02-09 13:16:50 -0800

    Finished tests for pxToRay

--- test/ray-spec.js
+++ test/ray-spec.js
@@ -28,9 +28,14 @@ describe("Converting pixels to rays", function () {
         // 1x1 window should center the ray on the z=-1 plane, starting zNear away
         // from the origin along the negative z axis
         var zNear = -1e-7;
-        var zray = new Ray(new Vector3(0, 0, zNear), new Vector3(0, 0, -1));
+        var ray = new Ray(new Vector3(0, 0, zNear), new Vector3(0, 0, -1));
 
-        expect(pxToRay(0, 0, 1, 1, zNear, 90)).toEqual(zray);
+        expect(pxToRay(0, 0, 1, 1, zNear, 90)).toEqual(ray);
+
+        // still works with big/far away near plane
+        zNear = -100;
+        ray = new Ray(new Vector3(0, 0, zNear), new Vector3(0, 0, -1));
+        expect(pxToRay(0, 0, 1, 1, zNear, 90)).toEqual(ray);
 
     });
 
@@ -40,30 +45,58 @@ describe("Converting pixels to rays", function () {
         var fieldOfViewX = 2 * Math.atan(2);
 
         // pixel 0,0 should be at -0.1,0.1 on z=-0.1 near plane.
-        var zray00 = new Ray(new Vector3(-0.1,0.1,-0.1), v3normalize(new Vector3(-1,1,-1)));
-        expect(pxToRay(0, 0, 2, 2, -0.1, fieldOfViewX)).toEqual(zray00);
+        var ray00 = new Ray(new Vector3(-0.1,0.1,-0.1), v3normalize(new Vector3(-1,1,-1)));
+        expect(pxToRay(0, 0, 2, 2, -0.1, fieldOfViewX)).toEqual(ray00);
 
         // pixel 0,1 at -0.1,-0.1.
-        var zray01 = new Ray(new Vector3(-0.1,-0.1,-0.1), v3normalize(new Vector3(-1,-1,-1)));
-        expect(pxToRay(0, 1, 2, 2, -0.1, fieldOfViewX)).toEqual(zray01);
+        var ray01 = new Ray(new Vector3(-0.1,-0.1,-0.1), v3normalize(new Vector3(-1,-1,-1)));
+        expect(pxToRay(0, 1, 2, 2, -0.1, fieldOfViewX)).toEqual(ray01);
 
         // pixel 1,0 at 0.1,0.1.
-        var zray01 = new Ray(new Vector3(0.1,0.1,-0.1), v3normalize(new Vector3(1,1,-1)));
-        expect(pxToRay(1, 0, 2, 2, -0.1, fieldOfViewX)).toEqual(zray01);
+        var ray01 = new Ray(new Vector3(0.1,0.1,-0.1), v3normalize(new Vector3(1,1,-1)));
+        expect(pxToRay(1, 0, 2, 2, -0.1, fieldOfViewX)).toEqual(ray01);
 
         // pixel 1,1 at 0.1,-0.1.
-        var zray01 = new Ray(new Vector3(0.1,-0.1,-0.1), v3normalize(new Vector3(1,-1,-1)));
-        expect(pxToRay(1, 1, 2, 2, -0.1, fieldOfViewX)).toEqual(zray01);
+        var ray01 = new Ray(new Vector3(0.1,-0.1,-0.1), v3normalize(new Vector3(1,-1,-1)));
+        expect(pxToRay(1, 1, 2, 2, -0.1, fieldOfViewX)).toEqual(ray01);
 
     });
 
-    it("4x2 window", function () {
-        // field of view is 90 degrees, so on z = -1 plane window will be scaled to [-1,1]x[-1/2,1/2].
-        // pixel 0,0 should be at (-3/4, 1/4), scaled accordingly.
-        var zray00 = new Ray(new Vector3(-0.75e-7, 0.25e-7, -1e-7),
-            new Vector3(-3 / Math.sqrt(26), 1 / Math.sqrt(26), 4 / Math.sqrt(26)));
-        expect(pxToRay(0, 0, 4, 2, -1e-7, 90)).toEqual(zray00);
+    it("4x2 window, narrow field of view", function () {
+        // field of view is going to be 90 degrees, so on z = -1 plane window will be scaled to [-1,1]x[-1/2,1/2].
+        // pixel 0,0 should be at (-0.075, 0.025) on z=-0.1 near plane
+        var ray00 = new Ray(new Vector3(-0.075, 0.025, -0.1),
+            v3normalize(new Vector3(-0.75, 0.25, -1)));
+        expect(pxToRay(0, 0, 4, 2, -0.1, Math.PI / 2)).toEqual(ray00);
 
+        // similarly for all 7 other pixels
+        var ray10 = new Ray(new Vector3(-0.025, 0.025, -0.1),
+            v3normalize(new Vector3(-0.25, 0.25, -1)));
+        expect(pxToRay(1, 0, 4, 2, -0.1, Math.PI / 2)).toEqual(ray10);
+
+        var ray20 = new Ray(new Vector3(0.025, 0.025, -0.1),
+            v3normalize(new Vector3(0.25, 0.25, -1)));
+        expect(pxToRay(2, 0, 4, 2, -0.1, Math.PI / 2)).toEqual(ray20);
+
+        var ray30 = new Ray(new Vector3(0.075, 0.025, -0.1),
+            v3normalize(new Vector3(0.75, 0.25, -1)));
+        expect(pxToRay(3, 0, 4, 2, -0.1, Math.PI / 2)).toEqual(ray30);
+
+        var ray01 = new Ray(new Vector3(-0.075, -0.025, -0.1),
+            v3normalize(new Vector3(-0.75, -0.25, -1)));
+        expect(pxToRay(0, 1, 4, 2, -0.1, Math.PI / 2)).toEqual(ray01);
+
+        var ray11 = new Ray(new Vector3(-0.025, -0.025, -0.1),
+            v3normalize(new Vector3(-0.25, -0.25, -1)));
+        expect(pxToRay(1, 1, 4, 2, -0.1, Math.PI / 2)).toEqual(ray11);
+
+        var ray21 = new Ray(new Vector3(0.025, -0.025, -0.1),
+            v3normalize(new Vector3(0.25, -0.25, -1)));
+        expect(pxToRay(2, 1, 4, 2, -0.1, Math.PI / 2)).toEqual(ray21);
+
+        var ray31 = new Ray(new Vector3(0.075, -0.025, -0.1),
+            v3normalize(new Vector3(0.75, -0.25, -1)));
+        expect(pxToRay(3, 1, 4, 2, -0.1, Math.PI / 2)).toEqual(ray31);
     });
 });
 
